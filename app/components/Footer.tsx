ff17e9048fb620c2f5b6c886335ef547b5ef77c6
perjinshavani <persha2008@hotmail.com>
2026-09-02 16:52:59 +0200

    Create basic FamilyMeal layout

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <footer  className="bg-pink-100 px-8 py-6 text-center">
+            <p className="text-sm text-zinc-700">
+                © 2026 Family Meal Planner. All rights reserved.</p>
+        </footer>
+    );
+}
