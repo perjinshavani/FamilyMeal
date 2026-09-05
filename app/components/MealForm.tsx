feb7ba388926621f4eae5e8575a861857df53e10
perjinshavani <persha2008@hotmail.com>
2026-09-05 19:40:03 +0200

    Add meal to weekly plan

---
+++ app/components/MealForm.tsx
@@ -0,0 +1,62 @@
+
+
+"use client";
+
+import { useState } from "react";
+
+type MealFormProps = {
+  onAddMeal: (day: string, meal: string) => void;
+};
+
+export default function MealForm({ onAddMeal }: MealFormProps) {
+  const [day, setDay] = useState("Monday");
+  const [meal, setMeal] = useState("");
+
+  const handleSubmit = () => {
+    if (!meal.trim()) return;
+
+    onAddMeal(day, meal);
+    setMeal("");
+  };
+
+  return (
+    <section>
+<h2 className="mb-5 text-2xl font-bold text-zinc-900">
+  Add Meal
+</h2>
+
+<div className="flex items-center gap-4">
+
+      <select
+        value={day}
+        onChange={(event) => setDay(event.target.value)}
+      >
+        <option>Monday</option>
+        <option>Tuesday</option>
+        <option>Wednesday</option>
+        <option>Thursday</option>
+        <option>Friday</option>
+        <option>Saturday</option>
+        <option>Sunday</option>
+      </select>
+
+      <input
+        type="text"
+        placeholder="Enter meal"
+        value={meal}
+        onChange={(event) => setMeal(event.target.value)}
+      />
+
+   <button
+  type="button"
+  onClick={handleSubmit}
+  className="rounded-lg bg-pink-500 px-4 py-2 font-semibold text-white transition hover:bg-pink-600 active:scale-95"
+>
+  Add
+</button>
+</div>
+    </section>
+    
+  );
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,31 @@
-import Header from './components/Header';
-import WeeklyMealPlan from './components/WeeklyMealPlan';
-import Footer from './components/Footer';
 
-export default function Home(){
-  return(
+"use client";
 
+import { useState } from "react";
+import MealForm from "./components/MealForm";
+import Header from "./components/Header";
+import WeeklyMealPlan from "./components/WeeklyMealPlan";
+import Footer from "./components/Footer";
+
+
+
+
+export default function Home() {
+  const [meals, setMeals] = useState<Record<string, string>>({});
+
+  const handleAddMeal = (day: string, meal: string) => {
+    setMeals((previousMeals) => ({
+      ...previousMeals,
+      [day]: meal,
+    }));
+  };
+
+  return (
     <main>
-
-<Header />
-<WeeklyMealPlan />
-<Footer />
+      <Header />
+      <MealForm onAddMeal={handleAddMeal} />
+      <WeeklyMealPlan meals={meals} />
+      <Footer />
     </main>
-
   );
 }
